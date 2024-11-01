b52df1e0b32e60b57de6902e65e8d9bbf5dbe541
Lucas Dominato <lucas.dominato@supplystack.com>
2024-11-01 12:48:09 +0000

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/lucasdominato/securefilemanager/SecureFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdominato.securefilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdominato/securefilemanager/SecureFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdominato.securefilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
